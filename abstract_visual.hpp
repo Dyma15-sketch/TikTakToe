38b753a0d4231f28ed552c691338bb9f97e7036c
Dyma15-sketch <dimaermurachi03@gmail.com>
2025-12-06 16:48:16 +0200

    Delete abstract_visual.hpp

--- abstract_visual.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "C:/Users/user/TikTakToe/include/board.hpp"
-#include <iostream>
-
-class AbstractVisual {
-public:
-    virtual void DrawBoard(const Board& board) const = 0;
-    virtual void ShowMessage(const std::string& message) const = 0;
-};
